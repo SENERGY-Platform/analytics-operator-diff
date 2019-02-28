607887ba4e0a731756aae0a3ee2d05d988efec3a
TZ <zsco@users.noreply.github.com>
2019-02-28 17:10:47 +0100

    change testing config

--- src/test/java/ValueDiffTest.java
+++ src/test/java/ValueDiffTest.java
@@ -38,12 +38,12 @@ public class ValueDiffTest {
     public void testRun(){
         valueDiff  = new ValueDiff();
         Builder builder = new Builder("1", "1");
-        JSONArray config = new JSONArray().put(new JSONObject().put("Name","test")
+        JSONObject config = new JSONObject().put("inputTopics", new JSONArray().put(new JSONObject().put("Name","test")
                 .put("FilterType", "DeviceId")
                 .put("FilterValue", "1")
                 .put("Mappings", new JSONArray().put(
                         new JSONObject().put("Source", "value.reading.value").put("Dest","value"))
-                ));
+                )));
         Message message = new Message(builder.formatMessage(ob1.toString()));
         message.setConfig(config.toString());
         valueDiff.config(message);
